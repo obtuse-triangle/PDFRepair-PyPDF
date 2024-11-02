72340dee20d4ac318b161a9c9f4a1022b90d0c53
obtuse-triangle <me@obtuse.kr>
2024-11-03 05:28:52 +0900

    add long_description_type

--- setup.py
+++ setup.py
@@ -21,5 +21,6 @@ setup(
         ]
     },
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/obtuse-triangle/PDFRepair-PyPDF"
 )
